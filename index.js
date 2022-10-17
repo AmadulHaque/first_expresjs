7d778d9811bfb7a8896abc81cebc0fe0d6a920a7
AmadulHaque <mdamadulhaque8@gmail.com>
2022-10-17 22:14:36 +0600

    multer firstcommit

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
 let express = require('express');
-app=express();
+let multer = require('multer');
+let app =express();
+ multer =multer();
 
 
-app.get("/bangladesh",function (req,res) {
-    res.redirect('http://localhost:8000/nepal');
-})
-
-app.get("/nepal",function (req,res) {
-    res.send('this nepal');
-})
-
-app.get("/six",function (req,res) {
-    res.append('name' ,'karim');
-    res.append('city' ,'dhaka');
-    res.append('age' ,'22');
-    res.status(201).end('this is body emo text');
-})
+app.use(multer.array());
+app.use(express.static('public'));
 
 
-app.get("/seven",function (req,res) {
-    res.cookie('name' ,'halim');
-    res.cookie('city' ,'dhaka');
-    res.cookie('age' ,'14');
-    res.end('this is body emo text');
-})
-app.get("/eight",function (req,res) {
-    res.clearCookie('name');
-    // res.clearCookie('city');
-    res.clearCookie('age');
-    res.end('this is body emo text');
+app.post('/',function (req,res){
+
+    let JSONdata = req.body;
+    let JSONstringify = JSON.stringify(JSONdata);
+    res.send(JSONstringify);
 })
 
 
